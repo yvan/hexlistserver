5a7ecaa08587f03fc6981a762c8fa970028f12a0
yvan <yvanscher@gmail.com>
2016-06-11 14:29:14 -0400

    changed to ssl

--- hexlistserver/config.py
+++ hexlistserver/config.py
@@ -13,8 +13,8 @@ class ProductionConfig(Config):
     DEBUG = False
     MAIL_SERVER=os.environ['MAIL_SERVER']
     MAIL_PORT=os.environ['MAIL_PORT']
-    MAIL_USE_TLS=True
-    MAIL_USE_SSL=False
+    MAIL_USE_TLS=False
+    MAIL_USE_SSL=True
     MAIL_USERNAME=os.environ['MAIL_USERNAME']
     MAIL_PASSWORD=os.environ['MAIL_PASSWORD']
 
